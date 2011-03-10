3aeb057754e75fa018531c217bef2590b847cba8
Mark Prins <1165786+mprins@users.noreply.github.com>
2011-03-10 22:57:51 +0000

    reformatting
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@128 5f327161-d4e9-4d76-8f34-ffb092c38ac6

--- icons/index.php
+++ icons/index.php
@@ -20,25 +20,30 @@
  *
  * @author Mark C. Prins
  */
- 
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl"
+	dir="ltr">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
-    <style type="text/css">
-        .box {width: 200px;float:left;padding: 0.5em;margin: 0;background-color: #FFFFFF;}
-    </style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<style type="text/css">
+.box {
+	width: 200px;
+	float: left;
+	padding: 0.5em;
+	margin: 0;
+	background-color: #FFFFFF;
+}
+</style>
 </head>
 <body>
-<div class="box">
-<?php
+<div class="box"><?php
 foreach (glob('*.png') as $img) {
-    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+	echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
 }
-?>
-</div>
+?></div>
 </body>
 </html>
