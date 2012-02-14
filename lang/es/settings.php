b78862567576ab521c939d93f19f1be4da5f9ef2
Mark Prins <1165786+mprins@users.noreply.github.com>
2012-02-14 13:01:55 +0000

    Spanish language files contributed by Txus
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@219 5f327161-d4e9-4d76-8f34-ffb092c38ac6

---
+++ lang/es/settings.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Copyright (c) 2008-2012 Mark C. Prins <mprins@users.sf.net>
+ * Copyright (c) 2012 Txus <txusff@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Spanish language file for the configuration manager.
+ *
+ * @author Mark C. Prins
+ * @author Txus 
+ */
+
+$lang['olScriptUrl'] = 'URL para acceso a OpenLayers API. Por defecto es DOKU_BASE/lib/plugins/openlayersmap/lib/OpenLayers.js';
+$lang['googleScriptUrl'] = 'URL para acceso a Google Maps API. <em>OBSOLETO por favor eliminalo. Reemplazado por la V3 de Google API</em>';
+$lang['veScriptUrl'] = 'URL & para acceso a Virtual Earth API. <em>OBSOLETO por favor eliminalo. Reemplazado por Bing<em>';
+//$lang['yahooScriptUrl'] = 'URL & cadena de consulta para acceder a Yahoo Maps API.';
+//no se necesita nunca más $lang['googleAPIKey'] = 'Google Maps API key.';
+$lang['bingAPIKey'] = 'Bing Maps API key. Consigue tu clave en: https://www.bingmapsportal.com/application';
+$lang['iconUrlOverload'] = 'URL para iconos cuando usas un mapa estático en uná máquina sin internet';
+$lang['enableMapQuest'] = 'Habilita MapQuest como proveedor de mapas';
+$lang['enableGoogle'] = 'Habilita Google como proveedor de mapas';
+$lang['enableBing'] = 'Habilita Bing como proveedor de mapas';
+$lang['enableOSM'] = 'Habilita OpenStreetMap como proveedor de mapas';
+$lang['olMapStyle'] = 'Estilo de los Controles del mapa';
+$lang['enableA11y'] = 'Habilita a11y/print para mapa estático, tabla de nombres';
