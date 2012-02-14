9c8c884ac32fbe91219c469bad7975b309fc7444
Mark Prins <1165786+mprins@users.noreply.github.com>
2012-02-14 13:20:28 +0000

    update copyright info
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@220 5f327161-d4e9-4d76-8f34-ffb092c38ac6

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2008-2011 Mark C. Prins <mc.prins@gmail.com>
+ * Copyright (c) 2008-2012 Mark C. Prins <mprins@users.sf.net>
 *
 * Permission to use, copy, modify, and distribute this software for any
 * purpose with or without fee is hereby granted, provided that the above
