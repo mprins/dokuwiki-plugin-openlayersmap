9c37410ded838dd46fe14c6929b4d74e1951c443
Mark Prins <1165786+mprins@users.noreply.github.com>
2016-11-13 11:20:27 +0000

    upgrade assembly plugin, add test stubs
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@711 5f327161-d4e9-4d76-8f34-ffb092c38ac6

---
+++ _test/general.test.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright (c) 2016 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * General tests for the openlayersmap plugin
+ *
+ * @group plugin_openlayersmap
+ * @group plugin_dokuwikispatial
+ * @group plugins
+ */
+class general_plugin_openlayersmap_test extends DokuWikiTest {
+
+    protected $pluginsEnabled = array('openlayersmap','geophp');
+
+    /**
+     * Simple test to make sure the plugin.info.txt is in correct format
+     */
+    public function test_plugininfo() {
+        $file = __DIR__.'/../plugin.info.txt';
+        $this->assertFileExists($file);
+
+        $info = confToHash($file);
+
+        $this->assertArrayHasKey('base', $info);
+        $this->assertArrayHasKey('author', $info);
+        $this->assertArrayHasKey('email', $info);
+        $this->assertArrayHasKey('date', $info);
+        $this->assertArrayHasKey('name', $info);
+        $this->assertArrayHasKey('desc', $info);
+        $this->assertArrayHasKey('url', $info);
+
+        $this->assertEquals('openlayersmap', $info['base']);
+        $this->assertRegExp('/^https?:\/\//', $info['url']);
+        $this->assertTrue(mail_isvalid($info['email']));
+        $this->assertRegExp('/^\d\d\d\d-\d\d-\d\d$/', $info['date']);
+        $this->assertTrue(false !== strtotime($info['date']));
+    }
+
+    /**
+     * test if plugin is loaded.
+     */
+    public function test_plugin_openlayersmap_isloaded() {
+        global $plugin_controller;
+        $this->assertTrue(
+                    in_array('geophp', $plugin_controller->getList()),
+                    "geophp plugin is loaded"
+                            );
+        $this->assertTrue(
+                    in_array('openlayersmap', $plugin_controller->getList()),
+                    "openlayersmap plugin is loaded"
+                            );
+    }
+}
