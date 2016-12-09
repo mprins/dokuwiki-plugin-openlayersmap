6921fdc470b4dbcfd7e415712840b28beb7fe7cf
Mark Prins <1165786+mprins@users.noreply.github.com>
2016-12-09 17:46:03 +0000

    add french translation by Digitalin
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@712 5f327161-d4e9-4d76-8f34-ffb092c38ac6

---
+++ lang/fr/lang.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2008-2015 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * French language file for openlayersmap plugin.
+ *
+ * @author Digitalin
+ *
+ * @license BSD license
+ */
+$lang ['olmapPOItitle'] = 'Points d\'Intérêts (POI)';
+$lang ['olmapPOIlatTitle'] = 'latitude en degré décimal';
+$lang ['olmapPOIlonTitle'] = 'longitude en degré decimal';
+$lang ['olmapPOIicon'] = 'symbole';
+$lang ['olmapPOIlat'] = 'latitude';
+$lang ['olmapPOIlon'] = 'longitude';
+$lang ['olmapPOItxt'] = 'description';
+$lang ['menu'] = 'Gestionnaire de cache Openlayersmap';
+$lang ['admin_tiles'] = 'Tuiles';
+$lang ['admin_purge_tiles'] = 'Purger la mise en cache des tuiles';
+$lang ['admin_maps'] = 'Cartes';
+$lang ['admin_purge_maps'] = 'Purger la mise en cache des cartes';
+$lang ['admin_submit'] = 'Purger';
+$lang ['admin_purged_tiles'] = 'La mise en cache des tuiles a été purgée.';
+$lang ['admin_purged_maps'] = 'La mise en cache des cartes a été purgée.';
+$lang ['alt_legend_poi'] = "";
+$lang ['alt_legend_kml'] = "Trace KML";
+$lang ['alt_legend_gpx'] = "Trace GPX ";
+$lang ['alt_legend_geojson'] = "objet GeoJSON";
---
+++ lang/fr/settings.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (c) 2008-2016 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * French language file for the configuration manager.
+ *
+ * @author Digitalin
+ */
+$lang['bingAPIKey'] = 'Clé d\'API Bing Maps. Obtenez votre clé chez: https://www.bingmapsportal.com/application';
+$lang['iconUrlOverload'] = 'URL pour les icônes de carte lors de l\'utilisation de cartes statiques sur un hôte non-Internet';
+$lang['enableStamen'] = 'Activer l\'utilisation de Stamen comme fournisseur de base de données cartographiques';
+$lang['enableGoogle'] = 'Activer l\'utilisation de Google comme fournisseur de base de données cartographiques';
+$lang['googleAPIkey'] = 'Clé d\'API Google Static Maps. Obtenez votre clé chez: https://code.google.com/apis/console';
+$lang['enableBing'] = 'Activer l\'utilisation de Bing comme fournisseur de base de données cartographiques';
+$lang['enableOSM'] = 'Activer l\'utilisation d\'OpenStreetMap comme fournisseur de base de données cartographiques';
+$lang['enableA11y'] = 'Activer a11y/Sortie d\'impression (carte statique, table des légendes)';
+$lang['optionStaticMapGenerator']='Compilateur de cartes statiques local ou distant. <b>L\'utilisation de \'local\' échoue lorsque le serveur ne peut pas accéder à Internet.</b>';
+$lang['autoZoomMap'] = 'Activer le zoom automatique pour la carte de secours';
+$lang['displayformat'] = 'Format d\'affichage des coordonnées; degrés décimaux ou degrés, minutes, secondes';
