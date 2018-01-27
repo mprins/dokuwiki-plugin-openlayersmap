783f8dfa2a01e1147f64a4eda0047878091a2388
Mark Prins <1165786+mprins@users.noreply.github.com>
2018-01-27 14:04:01 +0000

    Russian language files providd by Aleksej Kozlov
    git-svn-id: https://svn.code.sf.net/p/dokuwikispatial/code/trunk/openlayersmap/src@747 5f327161-d4e9-4d76-8f34-ffb092c38ac6

---
+++ lang/ru/lang.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Copyright (c) 2018 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Russian language file for openlayersmap plugin.
+ *
+ * @license BSD license
+ * @author  Aleksej Kozlov <nellesmeti@gmail.com>
+ */
+$lang ['olmapPOItitle'] = 'Интересные точки (POI)';
+$lang ['olmapPOIlatTitle'] = 'широта в градусах (с дробной частью)';
+$lang ['olmapPOIlonTitle'] = 'долгота в градусах (с дробной частью)';
+$lang ['olmapPOIicon'] = 'символ';
+$lang ['olmapPOIlat'] = 'широта';
+$lang ['olmapPOIlon'] = 'долгота';
+$lang ['olmapPOItxt'] = 'описание';
+$lang ['menu'] = 'Очистка кэша OpenLayersMap';
+$lang ['admin_tiles'] = 'Плитки';
+$lang ['admin_purge_tiles'] = 'Удалить все закэшированные плитки';
+$lang ['admin_maps'] = 'Карты';
+$lang ['admin_purge_maps'] = 'Удалить все закэшированные карты';
+$lang ['admin_submit'] = 'Очистить';
+$lang ['admin_purged_tiles'] = 'Закэшированные плитки были удалены.';
+$lang ['admin_purged_maps'] = 'Закэшированные карты были удалены.';
+$lang ['alt_legend_poi'] = "";
+$lang ['alt_legend_kml'] = "трек KML";
+$lang ['alt_legend_gpx'] = "трек GPX";
+$lang ['alt_legend_geojson'] = "объект GeoJSON";
---
+++ lang/ru/settings.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright (c) 2018 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Russian language file for the configuration manager.
+ *
+ * @license BSD license
+ * @author  Aleksej Kozlov <nellesmeti@gmail.com>
+ */
+$lang['bingAPIKey'] = 'Ключ для Bing Maps API. Получите ключ тут: https://www.bingmapsportal.com/application';
+$lang['tfApiKey'] = 'Ключ для Thunderforest API. Получите ключ тут: https://manage.thunderforest.com/dashboard';
+$lang['iconUrlOverload'] = 'URL для иконок карт (при использовании статических карт на не подключённом к интернету сервере)';
+$lang['enableStamen'] = 'Разрешить использование Stamen как провайдера карт';
+$lang['enableGoogle'] = 'Разрешить использование Google как провайдера карт';
+$lang['googleAPIkey'] = 'Ключ для Google Static Maps API. Получите ключ тут: https://code.google.com/apis/console';
+$lang['enableBing'] = 'Разрешить использование Bing как провайдера карт';
+$lang['enableOSM'] = 'Разрешить использование OpenStreetMap как провайдера карт';
+$lang['enableA11y'] = 'Разрешить вывод a11y/print (статическая карта, таблица с легендой)';
+$lang['optionStaticMapGenerator']='Локальное (local) или удалённое (remote) хранилище статических карт. <b>Локальное хранилище не будет работать, если сервер не подключён к интернету.</b>';
+$lang['autoZoomMap'] = 'Разрешить автоматическое масштабирование для выпадающей карты';
+$lang['displayformat'] = 'Формат отображения координат: DD - градусы с дробной частью, DMS - градусы, минуты, секунды';
